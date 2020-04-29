f895a82477031cb8e5a7e7421592ef1d1ac0df24
Evan Morrison <evan.morrison@tazworks.com>
2020-04-29 00:04:45 -0600

    refactor to be modifier for simple-translator; update build scripts

--- src/index.js
+++ src/index.js
@@ -1,91 +1,44 @@
 import React from 'react';
 import { isNil, isString, isNumber } from 'lodash';
 
-const Translator = {
-  defaultLanguageKey: null,
-  defaultLanguageLibrary: null,
+function translate(id, values = {}) {
+  const library = this.getLibrary();
+  if (typeof library === 'undefined') {
+    return id;
+  }
 
-  language: null,
+  let translation = this.findTranslation(library, id);
 
-  languages: {},
+  // defaults
+  if (isNil(translation) && this.language !== this.defaultLanguageKey) {
+    translation = this.findTranslation(this.getLibrary(this.defaultLanguageKey), id);
+  }
+  if (isNil(translation)) {
+    translation = id;
+  }
 
-  changeLanguage(language) {
-    if (this.languages.hasOwnProperty(language)) {
-      this.language = language;
-    } else {
-      this.language = this.defaultLanguageKey;
-    }
-  },
-
-  findTranslation(translation, id) {
-    const idPieces = id.split('.');
-
-    for (let i = 0; i < idPieces.length; i++) {
-      const piece = idPieces[i];
-      if (isNil(translation[piece])) {
-        return null;
-      } else {
-        translation = translation[piece];
-      }
-    }
-
-    return translation;
-  },
-
-  registerDefaultLanguage(key, library) {
-    this.defaultLanguageKey = key;
-    this.defaultLanguageLibrary = library;
-    if (this.language === null) {
-      this.language = this.defaultLanguageKey;
-    }
-    this.registerLanguage(key, library);
-  },
-
-  registerLanguage(key, library) {
-    const languages = this.languages;
-    languages[key] = library;
-    this.languages = languages;
-  },
-
-  translate(id, values = {}) {
-    const library = this.getLibrary();
-    if (typeof library === 'undefined') {
-      return id;
-    }
-
-    let translation = this.findTranslation(library, id);
-
-    // defaults
-    if (isNil(translation) && this.language !== this.defaultLanguageKey) {
-      translation = this.findTranslation(this.getLibrary(this.defaultLanguageKey), id);
-    }
-    if (isNil(translation)) {
-      translation = id;
-    }
-
-    function interpolate(part, values) {
-      if (isNumber(part)) return part;
-      if (isString(part)) {
-        for (const key in values) {
-          part = part.split('${' + key + '}').join(values[key]);
-        }
-        return part;
-      } else if (typeof part === 'function') {
-        part = part(values);
-        if (React.isValidElement(part)) {
-          const children = React.Children.map(part.props.children, child => interpolate(child, values));
-          return React.cloneElement(part, {}, ...children);
-        }
+  function interpolate(part, values) {
+    if (isNumber(part)) return part;
+    if (isString(part)) {
+      for (const key in values) {
+        part = part.split('${' + key + '}').join(values[key]);
       }
       return part;
+    } else if (typeof part === 'function') {
+      part = part(values);
+      if (React.isValidElement(part)) {
+        const children = React.Children.map(part.props.children, child => interpolate(child, values));
+        return React.cloneElement(part, {}, ...children);
+      }
     }
+    return part;
+  }
 
-    return interpolate(translation, values);
-  },
+  return interpolate(translation, values);
+}
 
-  getLibrary(language = this.language) {
-    return this.languages[language];
-  },
-};
+function ReactTranslator(Translator) {
+  Translator.translate = translate.bind(Translator);
+}
 
-export default Translator;
+export default ReactTranslator;
